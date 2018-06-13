308db42124c49b5017aaa8ef4541d5a6c27fa80a
Brittanie McDuffie <choc@Brittanies-MacBook-Pro.local>
2018-06-12 23:21:07 -0500

    almost there

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,48 +1,51 @@
-var confusion = ["b", "r", "q", "k", "f", "m", "x"];
+//Computer's choices
+var confusion = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
+//Define starting variables for the game
 var winner = 0;
 var loser = 0;
-var guesses = 0;
+var chances = 9;
+var guesses = []; 
 var choices = 0;
 
 document.onkeyup = function(event) {
 
-  // Determines which key was pressed.
-var alakazam = event.key; 
+  // Alakazam (user)'s guess
+    var alakazam = String.fromCharCode(event.keyCode);
 
-  // Randomly chooses a choice from the options array. This is the Computer's guess.
-var mewtwoGuess = confusion[Math.floor(Math.random() * confusion.length)];
+  // Mewtwo's guess :)
+    var mewtwoGuess = confusion[Math.floor(Math.random() * confusion.length)]; 
 
-  // Reworked our code from last step to use "else if" instead of lots of if statements.
+  // Logic to determine wins, losses, and display # of guesses and letters guessed
+  var alakazam = String.fromCharCode(event.keyCode).toLowerCase(); //taking in user guess
 
-  // This logic determines the outcome of the game (win/loss/tie), and increments the appropriate number
-  if ((alakazam === "b") || (alakazam === "r") || (alakazam === "q") (alakazam === "k") (alakazam === "f") (alakazam === "m") (alakazam === "x")) {
+        var mewtwoGuess = confusion[Math.floor(Math.random()*confusion.length)]; //computer selects random letter
+        guesses.push(alakazam); //pushing user guess to guesses so far
+        
+        if (alakazam == mewtwoGuess) {
+            wins++;
+            alert('Way to go! You\'ve guesesed corrrectly. You Won!');
+            chances = 9; //reseting the guesses back to 9 so that the user can play again
+            guesses.length = 0; //this removes everything from the guesses so far array, so that the guesses from the previous round don't show
+        }
+        else if (chances == 0){
+            losses++;
+            alert('You didn\'t guess the correct letter. You lost. Let\'s try again.');
+            chances = 9;
+            guesses.length = 0;
+        }
+        else if (alakazam !== mewtwoGuess){
+            chances--; //decrementing the guesses left
+        }  
 
-      if ((alakazam === "r") && (confusion === "s")) {
-      wins++;
-    } else if ((alakazam === "r") && (confusion === "p")) {
-      losses++;
-    } else if ((alakazam === "s") && (confusion === "r")) {
-      losses++;
-    } else if ((alakazam === "s") && (confusion === "p")) {
-      wins++;
-    } else if ((alakazam === "p") && (confusion === "r")) {
-      wins++;
-    } else if ((alakazam === "p") && (confusion === "s")) {
-      losses++;
-    } else if (alakazam === confusion) {
-      ties++;
-    }
-
-    // Creating a variable to hold our new HTML. Our HTML now keeps track of the user and computer guesses, and wins/losses/ties.
+    // Variable to link to the HTML
     var html =
       "<p>You chose: " + alakazam + "</p>" +
       "<p>The computer chose: " + confusion + "</p>" +
-      "<p>wins: " + wins + "</p>" +
-      "<p>losses: " + losses + "</p>" +
+      "<p>winner: " + winner + "</p>" +
+      "<p>loser: " + loser + "</p>" +
       "<p>ties: " + ties + "</p>";
 
     // Set the inner HTML contents of the #game div to our html string
     document.querySelector("#game").innerHTML = html;
-  }
-};
+  };
